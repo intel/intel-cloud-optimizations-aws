6646fe6ad83e715a46f86e552519ad3511fdf4cb
Eduardo Alvarez <57263404+eduand-alvarez@users.noreply.github.com>
2024-06-05 07:33:26 -0500

    Update setup.py

--- sagemaker/xgboost/pipeline-code/0_model-development/setup.py
+++ sagemaker/xgboost/pipeline-code/0_model-development/setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as f:
     readme = f.read()
 
 
-required_packages = ["sagemaker==2.93.0"]
+required_packages = ["sagemaker==2.214.3"]
 extras = {
     "test": [
         "black",
